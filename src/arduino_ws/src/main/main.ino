66bd669fc9eaab7b01ff3131f777949112c4b003
aransena <aransena@gmail.com>
2017-03-28 23:12:07 +0100

    ros bugs

--- src/arduino_ws/src/main/main.ino
+++ src/arduino_ws/src/main/main.ino
@@ -4,13 +4,16 @@
   27.03.17
   MIT License
 **/
-
+#if (ARDUINO >= 100)
+ #include <Arduino.h>
+#else
+ #include <WProgram.h>
+#endif
 #include <ros.h>
-#include <std_msgs/String.h>
-#include <std_msgs/Empty.h>
 #include <arduino_ws/Adc_Mega.h>
 #include <arduino_ws/LED_State.h>
 #include "FastLED.h"
+
 
 #define NUM_LDR 16
 #define NUM_LEDS 60
@@ -20,99 +23,63 @@
 #define UPDATES_PER_SECOND 100
 
 // prototypes
-void LEDControl( const arduino_ws::Adc_Mega& LEDctl_msg);
 void LDR_update(int *LDR_vals);
 
 // declare vars
 int LDR_vals[NUM_LDR];
 int LED_vals[NUM_LEDS];
 
-arduino_ws::LED_State led_msg;
-arduino_ws::Adc_Mega ldr_msg;
-
 ros::NodeHandle  nh;
-ros::Subscriber<arduino_ws::Adc_Mega> led_ctl("LDR_data", LEDControl );
-ros::Publisher led_pub("LED_data", &led_msg);
+
+arduino_ws::Adc_Mega ldr_msg;
 ros::Publisher ldr_pub("LDR_data", &ldr_msg);
 
 CRGB leds[NUM_LEDS];
 
-
-void LDR_update(int *LDR_vals) {
-  for (int i = 0; i < NUM_LDR; i++) {
-    LDR_vals[i] = analogRead(i);
+void LDR_update(arduino_ws::Adc_Mega &ldr_msg){//int *LDR_vals) {
+  for(int i=0; i<NUM_LDR; i++){
+    ldr_msg.data[i] = analogRead(i);
   }
+  
 }
 
-
-
-void LEDControl( const arduino_ws::Adc_Mega& LEDctl_msg) {
-  if (LEDctl_msg.data[0] > 200) {
-    digitalWrite(13, HIGH);
-  }
-  else {
-    digitalWrite(13, LOW);
-  }
-
-}
-
-
+long tstart;
 void setup()
 {
-  //delay(3000); //safety delay
-  FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS);
-
-  pinMode(13, OUTPUT);
   nh.initNode();
   nh.advertise(ldr_pub);
-  //nh.advertise(led_pub);
-  nh.subscribe(led_ctl);
 
-//  led_msg.red = malloc(NUM_LEDS);
-//  led_msg.green = malloc(NUM_LEDS);
-//  led_msg.blue = malloc(NUM_LEDS);
-//  
-//  led_msg.red_length = NUM_LEDS;
-//  led_msg.red = malloc(NUM_LEDS);
-//
-//  led_msg.green_length = NUM_LEDS;
-//  
-//  led_msg.blue_length = NUM_LEDS;
 
+  //delay(5000);
+  FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS);
+  
 }
-
-
 
 void loop()
 {
-  LDR_update(LDR_vals);
+  LDR_update(ldr_msg);//LDR_vals);
   
-  led_msg.id = 0;
-  led_msg.red = 1;
-  led_msg.green = 2;
-  led_msg.blue = 3;
   for(int i = 0; i < NUM_LDR; i++){
-    if(LDR_vals[i]<300){
+    if(ldr_msg.data[i]<300){//LDR_vals[i]
       leds[i]=CRGB::Green;
     }
-    else if(LDR_vals[i]<400){
+    else if(ldr_msg.data[i]<400){
       leds[i]=CRGB::Blue;
     }
-    else if(LDR_vals[i]>400){
+    else if(ldr_msg.data[i]>400){
       leds[i]=CRGB::Red;
     }
   }
-
-  memcpy(&ldr_msg.data, &LDR_vals, sizeof ldr_msg.data);
-
+  
+  
+//  delay(10);
+  
   ldr_pub.publish( &ldr_msg );
-  //led_pub.publish(&led_msg);
-
-
-//  led_pub.publish(&led_msg);
+//  
   nh.spinOnce();
+  delay(20);
   FastLED.show();
-  delay(10);
+  //delay(20);
 }
 
 
